3973079ba66e88287141f30051f4f9ad3a453814
chessa <chessachandrad@gmail.com>
2026-01-21 13:56:44 +0700

    soal2

---
+++ Quiz1_Yang_Seru_Sekali/Soal3.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  Random random = Random();
+  int angka = random.nextInt(100) + 1;
+
+  stdout.write("Tebak angka antara 1 sampai 100: ");
+  int input = int.parse(stdin.readLineSync()!);
+  int cobaan = 0;
+  while(input != angka){
+    if(input < angka){
+      print("Tebakan terlalu rendah. Coba lagi:");
+      cobaan += 1;
+    } else if(input > angka){
+      print("Tebakan terlalu tinggi. Coba lagi:");
+      cobaan += 1;
+    }else if(input == angka){
+      print("Yey! Tebakanmu benar, angkanya adalah $angka.");
+      print("Jumlah tebakan: $cobaan");
+    }
+    stdout.write("Masukkan tebakan baru: ");
+    input = int.parse(stdin.readLineSync()!);
+  }
+}
