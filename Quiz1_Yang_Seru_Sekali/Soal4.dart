419336f975921b670d23933f09b57ffa6fc4902d
chessa <chessachandrad@gmail.com>
2026-01-21 15:00:43 +0700

    fix

--- Quiz1_Yang_Seru_Sekali/Soal4.dart
+++ Quiz1_Yang_Seru_Sekali/Soal4.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 void main(){
+  List prima = [1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
+  List a = []; 
   stdout.write("Apakah ini bilangan prima: ");
   int input = int.parse(stdin.readLineSync()!);
-  if (input % input == 0 && input % 1 == 0 && input != 1 && input != 0 && input > 0 && input % 2 != 0 || input == 2 && input % 3 != 0) {
+  if (prima.contains(input)) {
     print("True");
   } else {
     print("False");
