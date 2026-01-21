3aebd36406fd4bcec89b368966bdbc0d38f2e6c1
chessa <chessachandrad@gmail.com>
2026-01-21 14:56:07 +0700

    soal8

---
+++ Quiz1_Yang_Seru_Sekali/Soal8.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  stdout.write("angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+  List<int> faktor = [];
+  for (var i = 1; i <= angka; i++) {
+    if (i != angka) {
+      if (angka % i == 0) {
+        faktor.add(i);
+      }
+    }
+  } 
+  int jumlah = 0;
+  for (var i in faktor) {
+   jumlah += i;
+   if (jumlah == angka) {
+     print("True");
+     break;
+   } else if (jumlah > angka) {
+     print("False");
+     break;
+   } 
+  } if (jumlah != angka) {
+    print("False");
+  }
+  print(faktor);
+}
